a78ba25ab03fd71c69a5e469f4d2574efb78edb1
btsutskiridze <93125681+btsutskiridze@users.noreply.github.com>
2022-07-19 00:50:45 +0400

    Update Insertion sort.cpp

--- Insertion sort.cpp
+++ Insertion sort.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 void insertionSort(vector<int>& data) {
-	for (int i = 0; i < data.size(); i++) {
+	for (int i = 1; i < data.size(); i++) {
 		int value = data[i];
 		int hole = i;
 		while (hole > 0 && data[hole - 1] > value) {
