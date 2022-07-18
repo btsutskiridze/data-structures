1aa5f5de00da8e6689fc4f5254e7929cfc8120d8
btsutskiridze <93125681+btsutskiridze@users.noreply.github.com>
2022-07-19 00:46:44 +0400

    Create Insertion sort.cpp

---
+++ Insertion sort.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void insertionSort(vector<int>& data) {
+	for (int i = 0; i < data.size(); i++) {
+		int value = data[i];
+		int hole = i;
+		while (hole > 0 && data[hole - 1] > value) {
+			data[hole] = data[hole - 1];
+			hole = hole - 1;
+		}
+		data[hole] = value;
+	}
+}
